09ed4018a678f3cf6f309b83e913d1edf2290c16
ricardo acevedo <ricardojracevedo@yahoo.com>
2015-04-15 15:03:13 -0700

    d

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -7,12 +7,17 @@ game.SpendExp = me.ScreenObject.extend({
                
                 me.game.world.addChild(new (me.Renderable.extend({
                     init: function(){
-                      this._super(me.Renderable, 'init', [270, 240, 300, 50 ]);
-                      this.font = new me.Font("arial", 46, "white");
+                      this._super(me.Renderable, 'init', [10, 10, 300, 50 ]);
+                      this.font = new me.Font("arial", 26, "white");
                     },
-                  
+                    //ADDING TEXT TO THE EXP SCREEN
                     draw: function(renderer){
-                        this.font.draw(renderer.getContext(), "SPEND", this.pos.x, this.pos.y);
+                        this.font.draw(renderer.getContext(), "PRESS F1-F4 TO BUY, F5 TO SKIP", this.pos.x, this.pos.y);
+                        this.font.draw(renderer.getContext(), "CURRENT EXP:" + game.data.exp.toString(), this.pos.x + 100, this.pos.y + 50);
+                        this.font.draw(renderer.getContext(), "F1: INCREASE GOLD PRODUCTION! CURRENT LEVEL" + game.data.exp1.toString() + " cost: " + ((game.data.exp1 + 1) * 10), this.pos.x, this.pos.y + 100);
+                        this.font.draw(renderer.getContext(), "F2: ADD STARTING GOLD" , this.pos.x, this.pos.y + 150);
+                        this.font.draw(renderer.getContext(), "F3: INCREASE ATTACK DAMAGE", this.pos.x, this.pos.y + 200);
+                        this.font.draw(renderer.getContext(), "F4: INCREASE STARTING HALTH", this.pos.x, this.pos.y + 250);
                     }, 
                    
                 })));
