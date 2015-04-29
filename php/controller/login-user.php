44e529bfe4ba41c5b0a2a7bdb4c0a490e2327400
ricardo acevedo <ricardojracevedo@yahoo.com>
2015-04-29 14:23:36 -0700

    jjr

--- php/controller/login-user.php
+++ php/controller/login-user.php
@@ -3,25 +3,32 @@
     require_once(__DIR__ . "/../model/config.php");
     
     $array = array(
-      'exp'=> '',  
-          'exp1'=> '', 
-        'exp2'=> '', 
-            'exp3'=> '', 
-        'exp4'=> '', 
+    'exp' => '',
+    'exp1' => '',
+    'exp2' => '',
+    'exp3' => '',
+    'exp4'=> '', 
     );
 
     //we are selecting user information from the database
     $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
     $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
-
-     $query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username' ");
+    $query = $_SESSION["connection"]->query("SELECT * FROM users WHERE username = '$username' ");
+    
       //checking the pasword for logins 
      if($query-> num_rows == 1){
          $rows = $query->fetch_array();
 
          if($row["password"] === crypt ($password, $row["salt"])){
              $_SESSION["authenticated"] = true;
-             echo "<p>Login Successfull!</p>";
+             $arrray["exp"] = $row["exp"];
+             $arrray["exp1"] = $row["exp"];
+             $arrray["exp2"] = $row["exp"];
+             $arrray["exp3"] = $row["exp"];
+             $arrray["exp4"] = $row["exp"];
+       
+             
+             echo json_encode($array);
          }
          else{
              echo "<p>Invalid username and password</p>";
