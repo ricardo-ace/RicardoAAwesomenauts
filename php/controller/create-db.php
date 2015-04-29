f71f300968861d4e624837e82e0674d409c7bad9
ricardo acevedo <ricardojracevedo@yahoo.com>
2015-04-29 15:02:58 -0700

    dd

--- php/controller/create-db.php
+++ php/controller/create-db.php
@@ -1,31 +1,7 @@
 <?php
     //prottecting the controllers and views 
     require_once(__DIR__ . "/../model/config.php");
-    require_once(__DIR__ . "/../controller/login-verify.php");
-    //killing the program  if not logged in 
-    if(!authenticateUser()){
-        header("location: " . $path . "index.php");
-        die();
-    }
-
-?>
-
-
-//this is to connect the variables i have in my database file to this folder
-require_once(__DIR__ . "/../model/config.php");
-
-//this is to create the post
-$query = $_SESSION["connection"]->query("CREATE TABLE posts ("
-        . "id int(11) NOT NULL AUTO_INCREMENT,"
-        . "title varchar(255) NOT NULL,"
-        . "post text NOT NULL,"
-        . "PRIMARY KEY (id))");
-
-if ($query) {
-    echo "<p>succesfully created table: posts</p>";
-} else {
-    echo"<p>" . $_SESSION["connection"]->error . "</p>";
-}
+  
 
  $query = $_SESSION["connection"]->query("CREATE TABLE users("
          . "id int(11) NOT NULL AUTO_INCREMENT,"
@@ -33,6 +9,11 @@ if ($query) {
          . "email varchar(50) NOT NULL,"
          . "password char(128)NOT NULL,"
          . "salt char(128) NOT NULL,"
+         . "exp int(4),"
+         . "exp1 int(4),"
+         . "exp2 int(4),"
+         . "exp3 int(4),"
+         . "exp4 int(4),"
          . "primary key(id))");
  
  if($query){
